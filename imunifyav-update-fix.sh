2e77abbc32ad6f9e31cf7a1d8e3b71ee0a317816
WarMax356 <111748232+Warmax356@users.noreply.github.com>
2025-08-09 01:36:30 +0200

    Create imunifyav-update-fix.sh
    a script to update ubuntu, autoclean and autoremove removed/depreciated files and fix permission for imunifyav UI

---
+++ imunifyav-update-fix.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade
+sudo apt autoclean -y
+sudo apt autoremove -y
+chmod +x /usr/local/CyberCP/public/imunifyav/classes/panels/../../bin/execute.py
